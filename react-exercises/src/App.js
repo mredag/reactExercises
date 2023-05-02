bf0ee44c8ad569f231a62af5a0dcb6d3863c2159
Emre <ct.edag@gmail.com>
2023-05-02 15:53:19 +0300

    exercise4  in react app

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Welcome from './welcome';
 
 function App() {
-  return (
-    <div className="App">
-      <Welcome name="Emre Dag" age={29} />
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Welcome name={<strong>Emre Doe</strong>} age={30} />
+      </div>
+    );
+  }
 
 export default App;
