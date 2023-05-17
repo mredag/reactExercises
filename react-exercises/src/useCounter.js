272bf6fb3a9b244ca7b0b8e471131d5e47a99c69
Emre <ct.edag@gmail.com>
2023-05-17 11:59:09 +0300

    39 done

--- react-exercises/src/useCounter.js
+++ react-exercises/src/useCounter.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 function useCounter(initialValue = 0) {
   const [count, setCount] = useState(initialValue);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(initialValue);
+  const increment = useCallback(() => setCount(prevCount => prevCount + 1), []);
+  const decrement = useCallback(() => setCount(prevCount => prevCount - 1), []);
+  const reset = useCallback(() => setCount(initialValue), [initialValue]);
 
   return { count, increment, decrement, reset };
 }
